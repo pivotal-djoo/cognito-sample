2ddb75ee46447f90c729831f54bb463fe793c797
Dave Joo <taehyunjoo@gmail.com>
2024-11-29 17:29:07 -0500

    Moved lambda functions to GitHub

--- functions/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs';
-
-export function populatePastField(reservations: Record<string, any>[]) {
-  return reservations.map((reservation) => ({
-    ...reservation,
-    past: dayjs(reservation.date).isBefore(Date.now()),
-  }));
-}
